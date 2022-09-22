f8c592985c6f526cece743b9d1fd6299936eccb1
Diego Quezada <71034913+dQuezada-P@users.noreply.github.com>
2022-09-22 12:12:02 -0300

    department routes

--- routes/routesDepartment.js
+++ routes/routesDepartment.js
@@ -9,6 +9,7 @@ const db = require("../config/config.js");
 //? verbos HTTP
 //* GET
 router.get("/all", async (req, res) => {
+  console.log('all')
   const sql = `BEGIN ACCIONES_DEPARTAMENTO.VER_DEPARTAMENTOS(:cursor); END;`;
 
   const binds = {
@@ -84,46 +85,7 @@ router.get("/", async (req, res) => {
   await db.Open(sql, binds, { isAutoCommit: false }, callback);
 });
 
-router.get("/", async (req, res) => {
-  const sql = `BEGIN ACCIONES_DEPARTAMENTO.VER_DEPARTAMENTO(:id,:cursor); END;`;
 
-  const binds = {
-    id: req.query.id,
-    cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT },
-  };
-
-  const callback = async (result) => {
-    const resultSet = result.outBinds.cursor;
-    rows = await resultSet.getRows();
-    await resultSet.close();
-    res.json(jsonListGen(rows))
-  };
-
-  const jsonListGen = (rows) => {
-
-    const json = []
-
-    rows.map((row)=>{
-      json.push({ 
-        id : row[0], 
-        nombre : row[1],
-        numero_banno : row[2],
-        numero_habitacion : row[3],
-        fecha : row[4],
-        direccion : row[5],
-        valor_arriendo : row[6],
-        ubicacion : row[7],
-        descripcion : row[8],
-      })
-    })
-    
-    console.log(json)
-    
-    return json
-  }
-
-  await db.Open(sql, binds, { isAutoCommit: false }, callback);
-});
 
 //* POST
 router.post("/", async (req, res) => {
@@ -131,38 +93,44 @@ router.post("/", async (req, res) => {
     nombre,
     numero_banno,
     numero_habitacion,
-    fecha,
     direccion,
     valor_arriendo,
     localidad,
     descripcion,
   } = req.body;
-
+  const dateNow = new Date()  
+  
+  const fecha = `${dateNow.getDate()}/${dateNow.getMonth()}/${dateNow.getFullYear()}`   
+  
   const binds = {
-    id: id,
     nombre: nombre,
-    numero_banno: numero_banno,
-    numero_habitacion: numero_habitacion,
+    numero_banno: parseInt(numero_banno,10),
+    numero_habitacion: parseInt(numero_habitacion,10),
     fecha: fecha,
     direccion: direccion,
-    valor_arriendo: valor_arriendo,
-    id_localidad: localidad,
+    valor_arriendo: parseInt(valor_arriendo,10),
+    id_localidad: parseInt(localidad,10),
     descripcion: descripcion,
     r: { type: oracledb.NUMBER, dir: oracledb.BIND_OUT },
+    msg: { type: oracledb.STRING, dir: oracledb.BIND_OUT }
   };
+  console.log(binds)
 
-  const sql = `BEGIN ACCIONES_DEPARTAMENTO.CREAR_DEPARTAMENTO(:id,
+  const sql = `BEGIN ACCIONES_DEPARTAMENTO.CREAR_DEPARTAMENTO(
                                                               :nombre,
                                                               :numero_banno,
                                                               :numero_habitacion,
-                                                              :fecha,:direccion,
+                                                              :fecha,
+                                                              :direccion,
                                                               :valor_arriendo,
                                                               :id_localidad,
                                                               :descripcion,
-                                                              :r); 
+                                                              :r,
+                                                              :msg); 
               END;`;
 
   const callback = (result) => {
+    console.log(result)
     res.json(result);
   };
   await db.Open(sql, binds, { isAutoCommit: true }, callback);
@@ -172,30 +140,31 @@ router.post("/", async (req, res) => {
 
 router.put("/", async (req, res) => {
   const {
-    id,
     nombre,
     numero_banno,
     numero_habitacion,
-    fecha,
     direccion,
     valor_arriendo,
     id_localidad,
     descripcion,
   } = req.body;
 
-  const sql = `BEGIN ACCIONES_DEPARTAMENTO.MODIFICAR_DEPARTAMENTO(:id,
+  const sql = `BEGIN ACCIONES_DEPARTAMENTO.MODIFICAR_DEPARTAMENTO(
+                                                              :id
                                                               :nombre,
                                                               :numero_banno,
                                                               :numero_habitacion,
-                                                              :fecha,:direccion,
+                                                              :fecha,
+                                                              :direccion,
                                                               :valor_arriendo,
                                                               :id_localidad,
                                                               :descripcion,
                                                               :r); 
               END;`;
 
+  const dateNow = Date.now()  
+  const fecha = `${dateNow.getDate()}/${dateNow.getMonth()}/${dateNow.getFullYear()}`            
   const binds = {
-    id: id,
     nombre: nombre,
     numero_banno: numero_banno,
     numero_Habitacion: numero_habitacion,
