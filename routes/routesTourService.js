a86953caf81db6c7653a35f77c0e6c606e789a31
nicolas6569 <ni.castillol@duocuc.cl>
2022-10-17 14:40:50 -0400

    update acciones tour view

--- routes/routesTourService.js
+++ routes/routesTourService.js
@@ -1,7 +1,7 @@
 //? Variables
 import {Router} from 'express'
 import oracledb from 'oracledb'
-import {conectBD} from '../config/config.js'
+import {conectBD, connectdb} from '../config/config.js'
 // const { Router } = require("express");
 // const oracledb = require("oracledb");
 const router = Router();
@@ -75,37 +75,55 @@ router.delete("/", async (req, res) => {
 });
 
 //*GET
-router.get("/", async (req, res) => {
+// router.get("/all", async (req, res) => {
+//   const binds = {
+//     cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT },
+//   };
+//   const sql = ``;
+
+//   const callback = async (result) => {
+//     const resultSet = result.outBinds.cursor;
+//     const rows = await resultSet.getRows();
+//     await resultSet.close();
+//     res.json(jsonListGen(rows));
+//   };
+//   const jsonListGen = (rows) => {
+//     console.log(rows);
+
+//     const json = [];
+
+//     rows.map((row) => {
+//       json.push({
+//         id: row[0],
+//         ciudad: row[1],
+//         cupo: row[2],
+//         precio: row[3],
+//         descripcion: row[4],
+//         horario: row[5],
+//       });
+//     });
+
+//     return json;
+//   };
+//   await conectBD(sql, binds, { isAutoCommit: true }, callback);
+// });
+router.get("/all", async (req, res) => {
+  const sql = `BEGIN ACCIONES_TOUR.VER_TOUR(:cursor);END;`;
+  
   const binds = {
     cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT },
   };
-  const sql = `BEGIN ACCIONES_TOUR.VER_TOUR(:cursor);END;`;
 
-  const callback = async (result) => {
-    const resultSet = result.outBinds.cursor;
-    const rows = await resultSet.getRows();
-    await resultSet.close();
-    res.json(jsonListGen(rows));
-  };
-  const jsonListGen = (rows) => {
-    console.log(rows);
+  const options = {
+    outFormat: oracledb.OUT_FORMAT_OBJECT,
+  }
 
-    const json = [];
+  const { cursor } = await connectdb(sql, binds, options);
 
-    rows.map((row) => {
-      json.push({
-        id: row[0],
-        ciudad: row[1],
-        cupo: row[2],
-        precio: row[3],
-        descripcion: row[4],
-        horario: row[5],
-      });
-    });
+  const tours = await cursor.getRows();
+  console.log(tours)
 
-    return json;
-  };
-  await conectBD(sql, binds, { isAutoCommit: true }, callback);
+  res.json(tours)
 });
 
 export default router
