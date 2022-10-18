9aec6fb144551bbc2abddb5112275bae397208c9
Lampiosko <claudio.abele@gmail.com>
2022-10-18 11:23:40 -0300

    v

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -1,26 +1,18 @@
 //Importamos Modelo
-import Tour from "../models/tour.model.js"
+import Tour from "../models/tour.model.js";
 
 export const getTours = async (req, res) => {
-  const tourModel = new Tour()
+  const tourModel = new Tour();
   const tourList = await tourModel.getTours();
-
-  res.json(tourList)
-}
+  res.json(tourList);
+};
 
 export const addTour = async (req, res) => {
   const { ciudad, cupo, precio, descripcion, horario } = req.body;
-  const tourModel = new Tour(
-    null,
-    ciudad,
-    cupo,
-    precio,
-    horario,
-    descripcion
-  );
-  
-  console.log(tourModel)
+  const tourModel = new Tour(null, ciudad, cupo, precio, horario, descripcion);
+
+  console.log(tourModel);
 
   const resultado = await tourModel.addTour();
   res.json(resultado);
-}
+};
