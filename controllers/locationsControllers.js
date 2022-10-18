892d961d32fc70451f8396b60f9eaabbc34289d4
Diego Quezada <71034913+dQuezada-P@users.noreply.github.com>
2022-10-18 13:01:05 -0300

    UPDATE

--- controllers/locationsControllers.js
+++ controllers/locationsControllers.js
@@ -2,7 +2,7 @@ import { getLocationsBD } from "../dao/utilsDao.js";
 
 export const getLocations = async (req, res) => {
   const states = await getLocationsBD()
-  states.length === 0 ? res.json({msg : 'No hay localidades'}) : res.json(states)
+  states?.length === 0 ? res.json({msg : 'No hay localidades'}) : res.json(states)
   
 };
 
