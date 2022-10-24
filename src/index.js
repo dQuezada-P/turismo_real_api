93fb03f25722149e4fad4d15afb38a6c6b73ed73
nicolas6569 <ni.castillol@duocuc.cl>
2022-10-23 20:17:09 -0400

    update index

--- src/index.js
+++ src/index.js
@@ -35,8 +35,8 @@ app.use(
 );
 
 //* routes
-app.use("/api/usuario", routerUser)
-app.use("/api/auth", routerAuth)
+app.use("/api/usuario", routerUser);
+app.use("/api/auth", routerAuth);
 app.use("/api/departamento/", routerDepartment);
 app.use("/api/utils", routerUtil);
 app.use("/api/inventario", routerInventary);
