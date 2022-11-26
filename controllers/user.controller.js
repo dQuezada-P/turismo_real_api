481e34846be94e992d0d43eb4b0d57e49a183466
lampiosko <54522763+lampiosko@users.noreply.github.com>
2022-11-25 22:45:40 -0300

    x

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,21 +1,23 @@
-import User from '../models/user.model.js'
+import { API_SECRET_KEY } from "../config/credentials.js";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import { emailConfirmAccount } from "../utils/email.js";
 
 export const getUsersNew = async (req, res) => {
   try {
     const { type_user } = req.query;
     let usersList;
-    if ( type_user == 'all'){
+    if (type_user == "all") {
       usersList = await new User().getUsers();
-    } else if ( type_user == 'client'){
+    } else if (type_user == "client") {
       usersList = await new User().getClients();
-    } else if ( type_user == 'employee') {
+    } else if (type_user == "employee") {
       usersList = await new User().getEmployees();
     } else {
-      res.status(400).json({ msg : 'No se entregó el tipo de usuario' })
+      res.status(400).json({ msg: "No se entregó el tipo de usuario" });
     }
 
-    res.json(usersList); 
-    
+    res.json(usersList);
   } catch (error) {
     console.error(error);
   }
@@ -23,9 +25,9 @@ export const getUsersNew = async (req, res) => {
 
 export const getUser = async (req, res) => {
   try {
-    console.log(req.query)
+    console.log(req.query);
     const { rut, correo } = req.query;
-    const user = await new User().getUser(rut, correo)
+    const user = await new User().getUser(rut, correo);
     res.json(user);
   } catch (error) {
     console.error(error);
@@ -33,39 +35,81 @@ export const getUser = async (req, res) => {
 };
 
 export const addUser = async (req, res) => {
-  const { rut, nombre, apellido, correo, direccion, telefono, password, id_rol } = JSON.parse(req.body.content);
-  console.log(JSON.parse(req.body.content))
-  console.log(req.files)
-  const newUser = new User(rut, nombre, apellido, req.files, correo, null, direccion, telefono, null, id_rol);
+  const {
+    rut,
+    nombre,
+    apellido,
+    correo,
+    direccion,
+    telefono,
+    password,
+    id_rol,
+  } = JSON.parse(req.body.content);
+  console.log(JSON.parse(req.body.content));
+  console.log(req.files);
+  const newUser = new User(
+    rut,
+    nombre,
+    apellido,
+    req.files,
+    correo,
+    null,
+    direccion,
+    telefono,
+    null,
+    id_rol
+  );
   newUser.pass = await newUser.encryptPassword(password);
 
   const response = await newUser.addUser();
 
-  res.json(response)
+  console.log(response)
+  const token = jwt.sign(
+    { id : response.r },
+    API_SECRET_KEY,
+    {expiresIn : 3600}
+  );
+
+  emailConfirmAccount({token,nombre,apellido,correo})
+  res.json(response);
 };
 
 export const editUser = async (req, res) => {
-  console.log(req.body)
-  const { rut, nombre, apellido, correo, estado, direccion, telefono, password } =
-    req.body;
-    
+  console.log(req.body);
+  const {
+    rut,
+    nombre,
+    apellido,
+    correo,
+    estado,
+    direccion,
+    telefono,
+    password,
+  } = req.body;
 
-  const newUser = new User(rut, nombre, apellido, 'imagen', correo, estado, direccion, telefono, null, null);
+  const newUser = new User(
+    rut,
+    nombre,
+    apellido,
+    "imagen",
+    correo,
+    estado,
+    direccion,
+    telefono,
+    null,
+    null
+  );
 
   const response = await newUser.editUser();
   res.json(response);
 };
 
-export const deleteUser = async(req, res) => {
-  try {       
-    const { rut } = req.params;    
-    const user = await new User().deleteUser(rut)
+export const deleteUser = async (req, res) => {
+  try {
+    const { rut } = req.params;
+    const user = await new User().deleteUser(rut);
     console.log(rut);
-    if (user == 0)
-      res.json({msg: "Usuario no existe"});
-    res.json(user);  
-  } catch (error) {
-    
-  }
- 
+    if (user == 0) res.json({ msg: "Usuario no existe" });
+    res.json(user);
+  } catch (error) {}
 };
